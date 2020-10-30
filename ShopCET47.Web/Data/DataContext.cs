f597f2a11cca313a461db58126efceb6d194cdfa
Tiago Lima <lima.tiago85@hotmail.com>
2020-10-30 23:29:00 +0000

    Db created and products controller

---
+++ ShopCET47.Web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopCET47.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCET47.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
